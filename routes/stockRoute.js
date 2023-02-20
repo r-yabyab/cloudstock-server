68092e238f96f9b8982db109fafdee6700d1eb8c
Scruffy pc <cayabyabrr@yahoo.com>
2023-02-19 23:27:14 -0800

    migrated URLS with key from FE to here

--- routes/stockRoute.js
+++ routes/stockRoute.js
@@ -4,68 +4,6 @@ const router = express.Router();
 
 const BASE_URL='https://ryabyab.iex.cloud/v1/data/core/quote/'
 
-// router.get("/list", async (req, res, next) => {
-//     console.log("/list call");
-//     try {
-//         const response = await axios.get('https://ryabyab.iex.cloud/v1/ref-data/iex/symbols?token=sk_4b6ebe9d84b44fe48cbf602d2c70884e');
-//         res.json(response.data);
-//     }
-//     catch(err) {
-//         next(err)
-//     }
-// })
-
-// router.get('/ticker', async (req, res, next) => {
-//     try {
-//         const response = await axios.get('https://ryabyab.iex.cloud/v1/data/core/quote/AAPL?token=sk_4b6ebe9d84b44fe48cbf602d2c70884e')
-//         res.json(response.data)
-//     }
-//     catch(err) {
-//         next(err)
-//     }
-// })
-
-
-
-// router.get('/stocklist', async (req, res, next) => {
-//     const stockList = req.query.yourstocks;
-
-
-
-
-//     if (Array.isArray(stockList)) {
-//         let tempStockData = [];
-    
-//         let promises = [];
-//         stockList.map((stock) => (
-//             promises.push(
-//                 axios
-//                     .get(`${BASE_URL}${stock.symbol}${TOKEN}`)
-//                     .then((response) => {
-//                         tempStockData.push({
-//                             symbol: stock.symbol,
-//                             id: stock.id,
-//                             ...response.data
-//                         });
-//                     })
-//                     .catch((error) => {
-//                         console.error("Error", error.message);
-//                     })
-//             )
-//         ));
-    
-//         Promise.all(promises)
-//             .then(() => {
-//                 res.send({ stockData: tempStockData });
-//             })
-//             .catch((error) => {
-//                 console.error("Error", error.message);
-//             });
-//     } else {
-//         next(new Error('Stock list is not an array'));
-//     }
-// });
-
 router.get('/tickers', async (req, res) => {
     try {
         const response = await fetch(`https://ryabyab.iex.cloud/v1/ref-data/iex/symbols${process.env.IEX_KEY}`);
@@ -77,6 +15,23 @@ router.get('/tickers', async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 })
+
+router.get('/tickerquote', async (req, res) => {
+    const {symbolName} = req.query;
+    // console.log(symbolName)
+
+    try {
+        const response = await fetch(`https://ryabyab.iex.cloud/v1/data/core/quote/${symbolName}${process.env.IEX_KEY}`);
+        const json = await response.json();
+
+        res.json(json);
+        // console.log(json)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
 
 router.get('/news', (req, res) => {
     // console.log(req.query.yourStocks)
@@ -122,50 +77,3 @@ router.get('/news', (req, res) => {
 
 
 module.exports = router;
-
-
-
-
-
-
-//// WORKS
-// router.get('/news', (req, res) => {
-//     // console.log(req.query.yourStocks)
-//     const getStockData = (stock) => {
-//             return axios
-//                 .get(`${BASE_URL}${stock.stock}${process.env.IEX_KEY}`)
-//                 .catch((error) => {
-//                     console.error("Error", error.message)
-//                 })
-//         }
-    
-//         // useEffect(() => {
-//             let tempStockData = []
-//             // const stockList = ["AAPL", "MSFT", "TSLA", "PCG", "AMZN"];
-//             // const stockList = (yourStocks.symbol)
-//             const stockList = req.query.yourStocks
-    
-    
-//             let promises = [];
-//             if (stockList) {
-//             stockList.map((stock) => (
-//                 promises.push(
-//                     getStockData(stock)
-//                         .then((res) => {
-//                             tempStockData.push({
-//                                 symbol: stock.stock,
-//                                 id: stock.id,
-//                                 // symbol: stock,
-//                                 ...res.data
-//                             })
-//                         })
-//                 )
-//             ))}
-    
-//             Promise.all(promises).then(() => {
-//                 // setStockData(tempStockData)
-//                 res.json(tempStockData)
-//             })
-    
-//         // }, [yourStocks, reducerValue])
-// })
